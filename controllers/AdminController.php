e3026c7d88a221c4d61d7b9d842bf2a9cb3319f8
respaldoisomigbreu-cmd <respaldo.iso.migbreu@gmail.com>
2026-04-26 21:46:11 -0400

    se activo la funcion isAdmin()

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -9,7 +9,7 @@ use MVC\Router;
 class AdminController {
     public static function index(Router $router){
         session_start();
-       //isAdmin();
+        isAdmin();
        // $nombre = $_SESSION['nombre'] ?? '';
 
 
