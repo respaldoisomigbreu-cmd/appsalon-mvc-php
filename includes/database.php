cd0c8aef3fa3fbb310052ecdc0408b5a2a09e561
respaldoisomigbreu-cmd <respaldo.iso.migbreu@gmail.com>
2026-04-26 20:00:29 -0400

    cambio en la base de dato (utf8mb4)

--- includes/database.php
+++ includes/database.php
@@ -7,7 +7,7 @@ $db = mysqli_connect(
                         $_ENV['DB_NAME'],
                     );    
     
-$db->set_charset('utf8');
+$db->set_charset('utf8mb4');
 
     if(!$db){
         echo "Error: no se pudo conectar a MySQL.";
