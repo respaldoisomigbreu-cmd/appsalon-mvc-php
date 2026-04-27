1001a924c18895e191f1e455d5268f32d85edf2a
respaldoisomigbreu-cmd <respaldo.iso.migbreu@gmail.com>
2026-04-26 22:02:54 -0400

    correccion ortografica del join en la manera de llamar la tabla citaservicios_1

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -29,7 +29,7 @@ class AdminController {
         $consulta .= " LEFT OUTER JOIN citasservicios ";
         $consulta .= " ON citasservicios.citaId=citas.id ";
         $consulta .= " LEFT OUTER JOIN servicios ";
-        $consulta .= " ON servicios.id=citasServicios.servicioId ";
+        $consulta .= " ON servicios.id=citasservicios.servicioId ";
         $consulta .= " WHERE fecha =  '{$fecha}' ";
 
         $citas = AdminCita::SQL($consulta);
